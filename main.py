6d1e683632af373e2d6d962d4d7291353a7ff7a3
zxc_322 <91989456+zxc322@users.noreply.github.com>
2022-05-18 13:30:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from human import Human
+from student import Student
+from group import Group
+
+human1 = Human('Artur', 'Johnson', 32)
+print(human1)
+student1 = Student('Cristiano', 'Ronaldo', 32, 'GO', 2, 4)
+print(student1)
+group1 = Group()
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+group1.add_student(student1)
+print('#' * 30)
+print(group1)
+
