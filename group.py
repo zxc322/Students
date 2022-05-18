8382eeffe53e308b0303dc3135e1d4ec7f2f4897
zxc_322 <91989456+zxc322@users.noreply.github.com>
2022-05-18 13:33:40 +0300

    Create group.py

---
+++ group.py
@@ -0,0 +1,45 @@
+class GroupIsFull(Exception):
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return 'This group already have 10 users!' + '\n' + "You can't add more students."
+
+
+class Group:
+
+    def __init__(self):
+        self.students_list = []
+
+    def add_student(self, new_student):
+        try:
+            if len(self.students_list) < 10:
+                self.students_list.append(new_student)
+            else:
+                raise GroupIsFull()
+        except GroupIsFull as err:
+            print(err)
+
+    def remove_student(self, student):
+        self.students_list.remove(student)
+
+    def search(self, last_name):
+        for i in self.students_list:
+            if last_name == i.last_name:
+                res = i
+                break
+            else:
+                res = 'Student with last name <' + last_name + '> not in group.'
+        return res
+
+    def converter(self):  #
+        res = ''
+        for i in self.students_list:
+            i = str(i)
+            res += i
+            res += '\n'
+        return res
+
+    def __str__(self):
+        return Group.converter(self)
