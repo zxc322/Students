b2c9be40fc34872d4450705e02f283fed36ab267
zxc_322 <91989456+zxc322@users.noreply.github.com>
2022-05-18 13:41:20 +0300

    Update group.py

--- group.py
+++ group.py
@@ -1,10 +1,4 @@
-class GroupIsFull(Exception):
-
-    def __init__(self):
-        super().__init__()
-
-    def __str__(self):
-        return 'This group already have 10 users!' + '\n' + "You can't add more students."
+from exception import GroupIsFull
 
 
 class Group:
